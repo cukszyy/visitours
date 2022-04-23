07147a5d5dd40dd9b68b47306dc49ad4c73d59be
Lucas Portella <lucas.portella.santos@santander.com.br>
2022-04-23 16:52:17 -0300

    Initial Commit

---
+++ Shared/VisitoursApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisitoursApp.swift
+//  Shared
+//
+//  Created by Lucas Portella Santos on 23/04/22.
+//
+
+import SwiftUI
+
+@main
+struct VisitoursApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
