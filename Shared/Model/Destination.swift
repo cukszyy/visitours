27f44f1f525e5cf3ec39e107bd105cf8ef2a3be7
Lucas Portella <lucas.portella.santos@santander.com.br>
2022-04-23 17:48:36 -0300

    Added Destination model and mocked data

---
+++ Shared/Model/Destination.swift
@@ -0,0 +1,14 @@
+//
+//  Destination.swift
+//  Visitours (iOS)
+//
+//  Created by Lucas Portella Santos on 23/04/22.
+//
+
+import SwiftUI
+
+struct Destination {
+    var place: String
+    var country: String
+    var image: String
+}
